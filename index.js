9c2ff04c19c7620bf06c0b4378bc2b3533d775bf
Avery Behm <darthavery23@gmail.com>
2022-09-05 15:55:03 -0600

    commit tree 89374d55c62056dc1aaaca5a8d56189d6c3afe70

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards, event) {
+    for (let i = 0; i < cards.length; i++) {
+        console.log(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+ 
+    return writeCards();
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
